09e2df1b5d930ff44c0a81c326f224fd66b41199
canersevince <canersevince@gmail.com>
2021-07-15 03:42:37 +0300

    init

--- routes/lib/thumbnail.js
+++ routes/lib/thumbnail.js
@@ -2,6 +2,6 @@ const {baseURI} = require('../../utils')
 const Pageres = require('pageres');
 
 module.exports = async (tokenId) => {
-    return await new Pageres({delay: 2, launchOptions: {headless: true, devtools: false, args: ['--no-sandbox','--disable-setuid-sandbox']}})
+    return await new Pageres({delay: 2, launchOptions: {headless: false, devtools: false, args: ['--no-sandbox','--disable-setuid-sandbox']}})
         .src(`${baseURI}/generator/${tokenId}`, ['400x400'], {crop: true}).run()
 }

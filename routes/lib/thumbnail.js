655b6f83316cb09f3f7813881166200e93a2cebc
canersevince <canersevince@gmail.com>
2021-07-15 04:22:43 +0300

    init

--- routes/lib/thumbnail.js
+++ routes/lib/thumbnail.js
@@ -2,7 +2,11 @@ const {baseURI} = require('../../utils')
 const puppeteer = require('puppeteer');         // Require Puppeteer module
 
 module.exports = async (tokenId) => {
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});    // Launch a "browser"
+    const browser = await puppeteer.launch({
+        defaultViewport: {width: 400, height: 400},
+        headless: true,
+        args: ['--no-sandbox']
+    });    // Launch a "browser"
     const page = await browser.newPage();        // Open a new page
     await page.goto(`${baseURI}/generator/${tokenId}`);                        // Go to the website
     let ss = await page.screenshot({                      // Screenshot the website using defined options

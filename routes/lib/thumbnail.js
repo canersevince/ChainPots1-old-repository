dfa61c1ad8a07864a92ebe4396f4a19847492358
canersevince <canersevince@gmail.com>
2021-07-15 04:14:35 +0300

    init

--- routes/lib/thumbnail.js
+++ routes/lib/thumbnail.js
@@ -2,7 +2,7 @@ const {baseURI} = require('../../utils')
 const puppeteer = require('puppeteer');         // Require Puppeteer module
 
 module.exports = async (tokenId) => {
-    const browser = await puppeteer.launch();    // Launch a "browser"
+    const browser = await puppeteer.launch({headless: true});    // Launch a "browser"
     const page = await browser.newPage();        // Open a new page
     await page.goto(`${baseURI}/generator/${tokenId}`);                        // Go to the website
     let ss = await page.screenshot({                      // Screenshot the website using defined options

9544dadd3b07fb07f5a85863e9cb15d91010eeb9
canersevince <canersevince@gmail.com>
2021-07-15 07:45:17 +0300

    init

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var {contract, baseURI} = require('../utils')
+var {contract, baseURI, mc} = require('../utils')
 var thumbnail = require('./lib/thumbnail')
 var getAttributes = require('../public/javascripts/metadata')
 /* GET home page. */
-let cached = {};
+
+
 router.get('/token/:id', async function (req, res, next) {
     try {
         let id = req.params.id
@@ -12,39 +13,25 @@ router.get('/token/:id', async function (req, res, next) {
             res.status(404)
             return
         }
-
-
-        if(cached[id]) {
-            res.status(200).json(cached[id])
-            console.log('served cache')
-            return
-        }
-
+        await mc.get(id, function (err, val) {
+            if (err != null) {
+                console.log('Error getting value: ' + err)
+            } else {
+                res.status(200).json(val)
+                return
+            }
+        })
         const hash = await contract.tokenHash(id)
         let metadata = getAttributes(hash)
         console.info('Creating Thumbnail')
-        // let image = await thumbnail(id)
-        // console.log(image)
-        // if (image) {
-        //     image = `https://ipfs.infura.io/ipfs/${image}`
-        // }
-
-        console.log({metadata})
-        cached[id] = {
-            tokenId: id,
-            image: "https://lh3.googleusercontent.com/DlPf9XbJw25Xo-9FEjIGrAGnUmJoJVaGp3_GC5cPXHH0tnw-WS3OiHXQKPCq9-8wEw6eWNNBZDjODuThd_QjC1ygacOlAftXJNpc=s302",
-            hash,
-            name: `Stairway To Chain - #${id}`,
-            description: `Generative NFT collection with limited supply and the scripts stored on Ethereum Blockchain. Inspired by on-chain art platform "Art Blocks".`,
-            animation_url: `${baseURI}/generator/${id}`,
-            token_uri: `${baseURI}/api/token/${id}`,
-            attributes: metadata || [],
-            external_url: `${baseURI}/token/${id}`,
-            script_type: "p5js",
-            aspect_ratio: "1",
+        let image = await thumbnail(id)
+        console.log(image)
+        if (image) {
+            image = `https://ipfs.infura.io/ipfs/${image}`
         }
-        res.status(200).json({
-            image: "https://lh3.googleusercontent.com/DlPf9XbJw25Xo-9FEjIGrAGnUmJoJVaGp3_GC5cPXHH0tnw-WS3OiHXQKPCq9-8wEw6eWNNBZDjODuThd_QjC1ygacOlAftXJNpc=s302",
+        console.log({metadata})
+        await mc.set(id, {
+            image,
             hash,
             name: `Stairway To Chain - #${id}`,
             description: `Generative NFT collection with limited supply and the scripts stored on Ethereum Blockchain. Inspired by on-chain art platform "Art Blocks".`,
@@ -54,12 +41,16 @@ router.get('/token/:id', async function (req, res, next) {
             external_url: `${baseURI}/token/${id}`,
             script_type: "p5js",
             aspect_ratio: "1",
+        }, {expires: 0}, function (err, val) {
+            if (err != null) {
+                console.log('Error setting value: ' + err)
+            }
+            res.status(200).json(val)
         })
     } catch (e) {
         console.log(e)
         res.status(404)
     }
-    return
 });
 
 module.exports = router;
--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
 var ethers = require('ethers');
 var abi = require('./abi.json')
+var memjs = require('memjs')
+
 const settings = {
     baseURI: "https://stairwaytochain.herokuapp.com",
     contractAddress: "0xAfFB101A5168ed99D7642746aC5390d8F7CF271E",
@@ -7,7 +9,14 @@ const settings = {
 }
 const provider = new ethers.providers.getDefaultProvider(settings.infuraAPI)
 const contract = new ethers.Contract(settings.contractAddress, abi, provider)
+
+var mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+    failover: true,  // default: false
+    timeout: 1,      // default: 0.5 (seconds)
+    keepAlive: true  // default: false
+})
 module.exports = {
     ...settings,
-    contract
+    contract,
+    mc
 }
